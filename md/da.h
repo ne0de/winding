817befd4845801491578b6c9cc001f2b6a74fe24
ne0de <nani.andres@hotmail.com.ar>
2021-10-16 01:54:59 -0300

    Update da.h

--- md/da.h
+++ md/da.h
@@ -3,13 +3,14 @@
 
 #include <iostream>
 
+template <class T>
 class DArray
 {
 private:
-    int *arr;
+    T *arr;
     size_t size;
     size_t cont;
-    const static size_t INC = 5;
+    const static size_t INC = 1;
 
     void expand();
 
@@ -17,7 +18,15 @@ public:
     DArray();
     ~DArray();
 
-    void insert(int value, bool back);
+    void insert(T value);
+    inline size_t csize();
+
+    T operator[](size_t position)
+    {
+        return arr[position];
+    }
 };
+
+#include "da.hxx"
 
 #endif
