7f14141f4cd10d7cc17601049d42399464f77b2f
ne0de <nani.andres@hotmail.com.ar>
2021-10-16 00:54:59 -0300

    Create datest.cpp

---
+++ md/datest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "da.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    DArray arr;
+    arr.insert(10, true);
+    arr.insert(0, true);
+    arr.insert(1, true);
+    arr.insert(4, true);
+    return 0;
+}
